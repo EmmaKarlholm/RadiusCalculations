7e4b5a5969cec198e1929a14b17e88e9fd14828c
Emma Karlholm <emma.karlholm@gmail.com>
2025-09-26 13:02:00 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadiusCalculations
+{
+    internal class Circle
+    {
+        public int Radius { get; set; }
+
+    }
+}
