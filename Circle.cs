e0e467caebd8983fb0223f36a0efc97f6c0a49e5
Emma Karlholm <emma.karlholm@gmail.com>
2025-09-26 14:16:18 +0200

    Added circumference

--- Circle.cs
+++ Circle.cs
@@ -10,5 +10,25 @@ namespace RadiusCalculations
     {
         public int Radius { get; set; }
 
+
+        public Circle(int radius)
+        {
+            Radius = radius;
+        }
+        public double GetCircumference()
+        {
+            return Radius * 2;
+        }
+
+        public double GetArea()
+        {
+            return Radius * Radius * Math.PI;
+        }
+
+        public double GetVolume()
+        {
+            return Radius * Radius * Radius;
+        }
+
     }
 }
