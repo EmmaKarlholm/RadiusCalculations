ed8de3560d6e6185ef23b631c47f07f957cb1f93
Emma Karlholm <emma.karlholm@gmail.com>
2025-09-27 15:18:13 +0200

    Added PositiveDouble method

--- UserInput.cs
+++ UserInput.cs
@@ -14,7 +14,7 @@ namespace RadiusCalculations
         {
             int value = 0;
             bool wasSuccessful = false;
-            
+
             bool stillValidating = true;
             while (stillValidating)
             {
@@ -30,7 +30,7 @@ namespace RadiusCalculations
                     stillValidating = false;
                 }
                 else
-                { 
+                {
                     Thread.Sleep(200);
                     Console.Write("Not a valid number. Try again: ");
                 }
@@ -51,6 +51,41 @@ namespace RadiusCalculations
                 {
                     string trimmedUserInput = userInput.Trim();
                     wasSuccessful = double.TryParse(trimmedUserInput, out value);
+                }
+
+                if (wasSuccessful)
+                {
+                    stillValidating = false;
+                }
+                else
+                {
+                    Thread.Sleep(200);
+                    Console.Write("Not a valid number. Try again: ");
+                }
+            }
+            return value;
+        }
+
+        public static double PositiveDouble()
+        {
+            double value = 0;
+            bool wasSuccessful = false;
+
+            bool stillValidating = true;
+            while (stillValidating)
+            {
+                string? userInput = Console.ReadLine();
+                if (userInput != null)
+                {
+                    string trimmedUserInput = userInput.Trim();
+                    bool wasNumber = double.TryParse(trimmedUserInput, out value);
+                    if (wasNumber)
+                    {
+                        if (value > 0)
+                        {
+                            wasSuccessful = true;
+                        }
+                    }
                 }
 
                 if (wasSuccessful)
